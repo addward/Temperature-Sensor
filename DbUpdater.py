66463601cbb39ca626120c8ed130eab76635709d
Aleksandr <adddward@gmail.com>
2023-11-26 16:45:28 +0000

    Updated to add new functions to work with DBases

--- DbUpdater.py
+++ DbUpdater.py
@@ -6,19 +6,34 @@ import os
 import sys
 
 from threading import Thread
-from secrets import db_port
+from secrets_local import db_port
+from datetime import datetime, timezone
 
+
+def readDB(dbname, request):
+    con = sqlite3.connect("./data/"+dbname)
+    cur = con.cursor()
+    result = cur.execute(request).fetchall()
+    con.commit()
+    cur.close()
+    con.close()
 
 def updateDB(request):
-    if ("dbname" not in request.keys() or 
-        "dsname" not in request.keys() or 
+    if ("dbname" not in request.keys() or
+        "dsname" not in request.keys() or
         len(request.keys()) < 3):
         print("wrong format")
         return
     dbname = request['dbname']
     dsname = request['dsname']
+
+    if 'datetime' in request.keys():
+        if len(request['datetime']) != 16:
+            request['datetime'] = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M")
+
     params = [elem for elem in request.keys() if elem not in ('dbname', 'dsname')]
     values = ["'{}'".format(request[elem]) for elem in params]
+
 
     print(request)
 
@@ -45,12 +60,12 @@ class DbUpdater:
 
     def __init__(self) -> None:
         self.__runningfl = True
-    
+
     def start(self) :
         self.thread = Thread(target=self.__RequestListener)
         self.thread.start()
         return True
-    
+
     def end(self) :
         self.__runningfl = False
         self.thread.join()
@@ -60,7 +75,7 @@ class DbUpdater:
         port = db_port  # initiate port no above 1024
         server_socket = socket.socket()  # get instance
         server_socket.settimeout(3.0)
-        
+
         server_socket.bind(("localhost", port))
         server_socket.listen(2)
 
